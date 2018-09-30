674fa4baa0710fd639fdfae5d7867341faced991
quan <quanhv@hblab.com>
2018-09-30 18:26:27 +0700

    m

--- client/src/views/Home/Login.js
+++ client/src/views/Home/Login.js
@@ -10,13 +10,14 @@ const Login = (props) => {
       appId="245691449626866"
       fields="name,email,picture"
       textButton="Facebook"
-      callback={(res) => props.loginFacebook(res.accessToken)}
+      callback={(res) => { props.loginFacebook(res.accessToken) }}
     />
     &nbsp;&nbsp;&nbsp;
     <GoogleLogin
       clientId="398331420373-ornoo96nrsd68a9tpritem5spl58p625.apps.googleusercontent.com"
       buttonText="Google"
-      onSuccess={(res) => props.loginGoogle(res.accessToken)}
+      onSuccess={(res) => { props.loginGoogle(res.accessToken) }}
+      onFailure={(e) => { console.log(e) }}
       className="google-login-button"
     />
   </div>
